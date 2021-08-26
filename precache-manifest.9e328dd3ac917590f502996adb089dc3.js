bc3e2d20670693b999ddbf84166cec09851c8b12
deve <tambatd@outlook.com>
2021-08-26 14:58:28 -0400

    Updates

---
+++ precache-manifest.9e328dd3ac917590f502996adb089dc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3453d9d297e2320707e1",
+    "url": "/JapanifyIO/static/css/main.14dca676.chunk.css"
+  },
+  {
+    "revision": "3453d9d297e2320707e1",
+    "url": "/JapanifyIO/static/js/main.c22dbdba.chunk.js"
+  },
+  {
+    "revision": "d7baa86abb0255bd9125",
+    "url": "/JapanifyIO/static/js/runtime~main.64f6f214.js"
+  },
+  {
+    "revision": "d0cf1624f979c000c179",
+    "url": "/JapanifyIO/static/css/2.b5a9f09e.chunk.css"
+  },
+  {
+    "revision": "d0cf1624f979c000c179",
+    "url": "/JapanifyIO/static/js/2.2e315997.chunk.js"
+  },
+  {
+    "revision": "fc405f01e7fe5cf6b33d8ae1fdb99aea",
+    "url": "/JapanifyIO/static/media/songs2.fc405f01.png"
+  },
+  {
+    "revision": "f7e6498fe958fa15963c4a9aa086d750",
+    "url": "/JapanifyIO/index.html"
+  }
+];
